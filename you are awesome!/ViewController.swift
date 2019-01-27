9bd71d707f1b47101eb445b862c408b38db31d97
Kyle Burns <kyleburns@kyles-macbook-pro-3.local>
2019-01-26 23:27:09 -0500

    2.14 completed - using declare but not initialize

--- you are awesome!/ViewController.swift
+++ you are awesome!/ViewController.swift
@@ -30,16 +30,39 @@ class ViewController: UIViewController {
                         "you brighten my day",
                         "you make me smile",
                         "I cant wait to download your app!"]
+        
+//        var newIndex = -1
+        var newIndex: Int //declares but doesnt initialize newIndex
+        
+        repeat {
+            newIndex = Int.random(in: 0 ..< messages.count )
+        } while index == newIndex
+        
+        index = newIndex
         messageLabel.text = messages[index]
         
         
-        if index == messages.count - 1 {
-            index = 0
-        } else {
-            index = index + 1
-        }
         
-        {}
+        
+        
+        
+        
+//        messageLabel.text = messages.randomElement()!
+        
+        
+        
+        
+        
+        
+//        messageLabel.text = messages[index]
+//
+//
+//        if index == messages.count - 1 {
+//            index = 0
+//        } else {
+//            index = index + 1
+        }
+    
         
 //        let message1 = "you are awesome"
 //        let message2 = "you are great"
@@ -55,5 +78,5 @@ class ViewController: UIViewController {
         
     }
 
-}
+
 
